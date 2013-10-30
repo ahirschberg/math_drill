6d22a6dead7bc5f9203baf7ad2c597e673fe7bd3
Alex <googlestinks@ymail.com>
2013-10-30 11:33:47 -0400

    Updated run.py to load names of students

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ import os, bottle, random
 curdir =  os.path.dirname(os.path.abspath(__file__))
 bottle.TEMPLATE_PATH.insert(0, curdir + '/template/')
 
+students = [line.strip() for line in open('students.txt')]
+ 
 print(">>>>>>>>>>>>>>" + curdir)
 
 @route('/')
 def index():
-    return send_static('index.html')
+    return server_static('index.html')
 
 @route('/test')
 @route('/test/<title>')
@@ -18,7 +20,12 @@ def index(title='Test'):
 
 @route('/drill')
 def generate_question():
-    return template('drill.tpl', imgpath="/static/img/" + random_image(), imgalt="Question", question="What is your opinion on airplane wings?")
+    return template('drill.tpl', imgpath="/static/img/" + random_image(), imgalt="Question", question=random_student() + ", what is your opinion on airplane wings?")
+
+def random_student():
+    rand = random.randrange(len(students))
+    return students[rand]
+    
 
 def random_image():
     filelist = os.listdir(curdir + "/static/img/") 
